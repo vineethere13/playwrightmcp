7513b47f032da9d9857640cf9fb146357fbb72b9
vineethere13 <vineethere13@gmail.com>
2026-05-17 21:59:20 -0400

    Centralized all Locators and used them in code

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -33,7 +36,9 @@ export default defineConfig({
     trace: 'on-first-retry',
 
     /* Take screenshot only on failure */
-    screenshot: { mode: 'only-on-failure', dir: './screenshots' },
+     headless: true,
+     video: 'retain-on-failure',
+    screenshot: { mode: 'only-on-failure' },
   },
 
   /* Configure projects for major browsers */
